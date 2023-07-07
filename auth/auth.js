aa8225d3674957a5291ceda9a487cf91f46a6f66
Suraj K J <93546991+srj-kj@users.noreply.github.com>
2023-07-07 10:29:52 +0530

    jwt fixed

--- auth/auth.js
+++ auth/auth.js
@@ -7,7 +7,7 @@ dotenv.config();
 export const generateAccessToken = async (userId) => {
     try {
         
-        return await jwt.sign({  userId: userId.toString() }, process.env.JWT_TOKEN_SECRET, { expiresIn: '2d' })
+        return await jwt.sign({  userId: userId.toString() }, process.env.JWT_TOKEN_SECRET, { expiresIn: '30d' })
     } catch (error) {
         console.log("error from JWT auth", error);
         throw { error }
